1e465d52b3505832bd9b12ce2e75e6fab2ea839d
Maaiz Shaikh <88193033+Maaiz-Shaikh@users.noreply.github.com>
2022-08-24 21:02:47 +0530

    Uploaded lib, android, ios, pubspec.yaml

---
+++ lib/models/task_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'dart:collection';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy bread'),
+    Task(name: 'Buy eggs'),
+  ];
+
+  // Getter
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  void addTask(String newTask) {
+    _tasks.add(Task(name: newTask));
+    notifyListeners();
+  }
+
+  void updataTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
